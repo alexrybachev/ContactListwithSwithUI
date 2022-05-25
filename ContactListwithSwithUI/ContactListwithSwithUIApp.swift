53b2b2c20d4e8e7e0a9cd5707101e397d8b8dd1d
Alex Rybachev <rybachev.as@gmail.com>
2022-05-25 22:05:09 +0400

    Initial Commit

---
+++ ContactListwithSwithUI/ContactListwithSwithUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListwithSwithUIApp.swift
+//  ContactListwithSwithUI
+//
+//  Created by Aleksandr Rybachev on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListwithSwithUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
